7b0fdbcb65ff3149abdba98ff93735ed33c774c4
TorchAblaze <tiffanygreathead@gmail.com>
2021-07-20 11:51:55 -0700

    Add Cors to Startup.cs
    Co-authored-by: Jeremy Banka <jeremybanka@gmail.com>
    Co-authored-by: James Wyn <thisisjameswyn@gmail.com>
    Co-authored-by: Michael Burton  <micbur1@gmail.com>
    Co-authored-by: Min Chang <minchangmhc@gmail.com>

--- RestServer/Startup.cs
+++ RestServer/Startup.cs
@@ -17,49 +17,60 @@ using RestServer.Models;
 
 namespace RestServer
 {
-    public class Startup
+  public class Startup
+  {
+    readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-
-            services.AddControllers();
-            services.AddDbContext<RestServerContext>(options => options
-                .UseMySql(Configuration["ConnectionStrings:DefaultConnection"], 
-                ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "animal_shelter", Version = "v1" });
-            });
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "animal_shelter v1"));
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
+      Configuration = configuration;
     }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+
+      services.AddControllers();
+      services.AddDbContext<RestServerContext>(options => options
+          .UseMySql(Configuration["ConnectionStrings:DefaultConnection"],
+          ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
+      services.AddCors(options =>
+        {
+          options.AddPolicy(name: MyAllowSpecificOrigins,
+                builder =>
+                {
+                  builder.WithOrigins("http://localhost:3000/");
+                });
+        });
+      services.AddSwaggerGen(c =>
+      {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "animal_shelter", Version = "v1" });
+      });
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+        app.UseSwagger();
+        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "animal_shelter v1"));
+      }
+
+      app.UseCors(MyAllowSpecificOrigins);
+
+      app.UseHttpsRedirection();
+
+      app.UseRouting();
+
+      app.UseAuthorization();
+
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllers();
+      });
+    }
+  }
 }
